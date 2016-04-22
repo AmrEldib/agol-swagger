2c4d76a022aed56ea9e90373aafaed1317acf87c
Amr Eldib <eldib.amr@gmail.com>
2016-04-21 20:06:26 -0700

    Modified swagger file to make sure it's valid
    Split some definitions of body parameters into their own file. Add step to updateDefs build

--- build/update-swagger-definitions.js
+++ build/update-swagger-definitions.js
@@ -4,12 +4,21 @@ var path = require('path');
 var fs = require('fs');
 
 agolSchemas.getSchema('swaggerDefinitions').then(function (defs) {
-  fs.readFile(path.resolve(__dirname, '..', 'data/swagger.json'), function (error, data) {
+  fs.readFile(path.resolve(__dirname, '..', 'data/swagger.json'), 'utf8', function (error, data) {
     if (error) throw error;
     var swaggerDoc = JSON.parse(data);
     swaggerDoc.definitions = defs;
-    fs.writeFile(path.resolve(__dirname, '..', 'data/swagger.json'), JSON.stringify(swaggerDoc, null, 2), function () {
-      console.log('Swagger definitions updated');
+    
+    fs.readFile(path.resolve(__dirname, '..', 'data/body-parameters.json'), 'utf8', function (err, data) {
+      var bodyParametersDefs = JSON.parse(data);
+      Object.keys(bodyParametersDefs.definitions).forEach(function (key) {
+        swaggerDoc.definitions[key] = bodyParametersDefs.definitions[key];
+      });
+      
+      fs.writeFile(path.resolve(__dirname, '..', 'data/swagger.json'), JSON.stringify(swaggerDoc, null, 2), function () {
+        console.log('Swagger definitions updated');
+      });
+
     });
   });
 });
